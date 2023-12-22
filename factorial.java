2c4dcea8108efcb938addb47e3419d8fa246946e
dandupraveen <praveenvenky574@gmail.com>
2023-12-22 17:06:18 +0530

    added new file

---
+++ factorial.java
@@ -0,0 +1,13 @@
+class Factorial
+{
+	public static void main(String args[])
+	{
+		int num=5,num2=1,factorial=1
+		while(num>=num2)
+		{
+			 factorial=factorial*num; 
+			num--;
+		}
+			System.out.println(factorial);	
+	}
+}
